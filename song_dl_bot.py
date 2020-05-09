2d74e60b92cdc71a1db13a29202264654a10b486
YuvalBarak10 <31255153+YuvalBarak10@users.noreply.github.com>
2020-05-10 02:19:30 +0300

    Update song_dl_bot.py

--- song_dl_bot.py
+++ song_dl_bot.py
@@ -205,7 +205,7 @@ def get_link(update, context):
             audio=open(items, "rb"),
             title=song.artist.name + ' - ' + song.title ,
             performer='ראשונים במוזיקה',
-            thumb=rsz_img_20200510_015426_839.jpg,
+            thumb=open('rsz_img_20200510_015426_839.jpg', 'rb'),
         )
         
         track = {
